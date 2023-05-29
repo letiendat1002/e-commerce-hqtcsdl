47a5932d3533f844149c01f9e3904cc0af51fac7
enpysen <20h1120150@ut.edu.vn>
2023-05-29 15:28:15 +0700

    Edit code, create role of user and employee for tables

--- src/decentralization.sql
+++ src/decentralization.sql
@@ -1,39 +1,179 @@
-USE myecommerce
-GO
+use myecommerce
+go
 
---Create user
+-- Create user
 create login user_test with password = '1234'
 create user user_test from login user_test
--- Read and update profile
-create view my_profile as select * from UserAccount
-GRANT SELECT ON [dbo].[my_profile] TO [user_test]
+
+-- Create role user
+USE [myecommerce]
 GO
-GRANT UPDATE ON [dbo].[UserAccount] TO [user_test]
+CREATE ROLE [user]
+GO
+USE [myecommerce]
+GO
+ALTER AUTHORIZATION ON SCHEMA::[db_owner] TO [user]
+GO
+use [myecommerce]
+GO
+DENY UPDATE ON [dbo].[UserOrder] ([DateOrder]) TO [user]
+GO
+use [myecommerce]
+GO
+DENY UPDATE ON [dbo].[UserOrder] ([Status]) TO [user]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[UserOrder] ([Address]) TO [user]
+GO
+use [myecommerce]
+GO
+GRANT DELETE ON [dbo].[UserOrder] TO [user]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[UserOrder] TO [user]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[UserOrder] TO [user]
+GO
+use [myecommerce]
+GO
+DENY UPDATE ON [dbo].[UserOrder] ([UserID]) TO [user]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[UserAccount] TO [user]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[UserAccount] TO [user]
+GO
+use [myecommerce]
+GO
+DENY UPDATE ON [dbo].[UserOrder] ([OrderID]) TO [user]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[UserOrder] ([PaymentType]) TO [user]
+GO
+use [myecommerce]
+GO
+DENY UPDATE ON [dbo].[UserOrder] ([AdditionalPrice]) TO [user]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[OrderDetail] TO [user]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[OrderDetail] TO [user]
 GO
 
--- Dat hang va mua hang
-GRANT DELETE ON [dbo].[UserOrder] TO [user_test]
-GO
-GRANT INSERT ON [dbo].[UserOrder] TO [user_test]
-GO
-GRANT SELECT ON [dbo].[UserOrder] TO [user_test]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([AdditionalPrice]) TO [user_test]
-GO
-GRANT UPDATE ON [dbo].[UserOrder] ([PaymentType]) TO [user_test]
-GO
-GRANT UPDATE ON [dbo].[UserOrder] ([Address]) TO [user_test]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([Status]) TO [user_test]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([UserID]) TO [user_test]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([OrderID]) TO [user_test]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([DateOrder]) TO [user_test]
-GO
-GRANT INSERT ON [dbo].[OrderDetail] TO [user_test]
-GO
-GRANT SELECT ON [dbo].[OrderDetail] TO [user_test]
+
+-- Add member
+ALTER ROLE [user] ADD MEMBER [user_test]
 GO
 
+-- Create employee_table
+create login employee_product with password = '1234'
+create user employee_product from login employee_product
+
+-- Create role employee
+USE [myecommerce]
+GO
+CREATE ROLE [employee]
+GO
+USE [myecommerce]
+GO
+ALTER AUTHORIZATION ON SCHEMA::[db_owner] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[OrderDetail] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[OrderDetail] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[OrderDetail] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT DELETE ON [dbo].[Category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[Category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[Category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[Category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT DELETE ON [dbo].[Product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[Product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[Product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[Product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT DELETE ON [dbo].[UserAccount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[UserAccount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[UserAccount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[UserAccount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT DELETE ON [dbo].[UserOrder] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT INSERT ON [dbo].[UserOrder] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT SELECT ON [dbo].[UserOrder] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT UPDATE ON [dbo].[UserOrder] TO [employee]
+GO
+
+
+-- Add member in employee's role
+USE [myecommerce]
+GO
+ALTER ROLE [employee] ADD MEMBER [employee_product]
+GO
+
+
+-- Create admin_system
+create login admin_test with password = '1234'
+create user admin_test from login admin_test
