edff3ea4d651f5fcc0982e755b2e7a77d969ad5b
Dat Tien Le <letiendat1002@gmail.com>
2023-05-31 21:48:40 +0700

    complete backup-restore

--- src/backup.sql
+++ src/backup.sql
@@ -1,23 +1,52 @@
---FUll backup vao CN
+USE myecommerce
+GO
+--Full backup
 BACKUP DATABASE myecommerce -- file 1
-TO DISK = N'C:\backup\Backup.bak';
+TO DISK = N'C:\backup\Backup.bak'
+WITH FORMAT, DESCRIPTION = 'Ecommerce FULL Backup'
+GO
 
 -- differ backup 
 BACKUP DATABASE myecommerce -- file 2
 TO DISK = N'C:\backup\Backup.bak'
-WITH DIFFERENTIAL;
+WITH DIFFERENTIAL, DESCRIPTION = 'Ecommerce Diff Backup'
+GO
 
---Phuc hoi full
+-- transaction log backup
+BACKUP LOG myecommerce
+TO DISK = N'C:\backup\Backup.trn'
+WITH DESCRIPTION = 'Ecommerce Log Backup'
+
+-- auto backup transaction log sau moi transaction
+
+--Restore full ve trang thai ban dau
+USE master
+GO
+DROP DATABASE myecommerce
+GO
+
 RESTORE DATABASE myecommerce
 FROM DISK = N'C:\backup\Backup.bak'
-WITH FILE = 1, REPLACE, RECOVERY;
+WITH FILE = 1, REPLACE, RECOVERY
+GO
 
+-- Restore voi cac transaction
+USE master
+GO
+DROP DATABASE myecommerce
+GO
 
--- Phuc hoi Diff
 RESTORE DATABASE myecommerce
 FROM DISK = N'C:\backup\Backup.bak'
-WITH FILE = 1, REPLACE, NORECOVERY;
+WITH FILE = 1, REPLACE, NORECOVERY
+GO
 
 RESTORE DATABASE myecommerce
 FROM DISK = N'C:\backup\Backup.bak'
-WITH FILE = 2, RECOVERY;
+WITH FILE = 2, REPLACE, NORECOVERY
+GO
+
+RESTORE LOG myecommerce
+FROM DISK = N'C:\backup\Backup.trn'
+WITH RECOVERY
+GO
