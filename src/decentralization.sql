5aff49f034027ef1a85af64f429656aa512ef8f6
enpysen <20h1120150@ut.edu.vn>
2023-06-01 00:05:29 +0700

    reset role customer & employee

--- src/decentralization.sql
+++ src/decentralization.sql
@@ -1,245 +1,295 @@
 use myecommerce
 go
 -----------------
--- Create customer --
+-- Create user --
 -----------------
 create login user_test with password = '1234'
 create user user_test from login user_test
 
--- Create role user
+create login employee_product with password = '1234'
+create user employee_product from login employee_product
+
+create login admin_test with password = '1234'
+create user admin_test from login admin_test
+
+-- Create role customer
 USE [myecommerce]
 GO
 CREATE ROLE [customer]
 GO
-USE [myecommerce]
-GO
-ALTER AUTHORIZATION ON SCHEMA::[guest] TO [customer]
-GO
 
--- customer's role invole table
+-- Set role customer
 use [myecommerce]
 GO
-DENY UPDATE ON [dbo].[UserOrder] ([DateOrder]) TO [customer]
-GO
-use [myecommerce]
-GO
-DENY UPDATE ON [dbo].[UserOrder] ([Status]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_select_order_by_orderid] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT UPDATE ON [dbo].[UserOrder] ([Address]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_select_user_account_by_phone] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT DELETE ON [dbo].[UserOrder] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_update_user_account_info_by_phone] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT INSERT ON [dbo].[UserOrder] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_delete_user_address] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT SELECT ON [dbo].[UserOrder] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_delete_all_order_by_userid] TO [customer]
 GO
 use [myecommerce]
 GO
-DENY UPDATE ON [dbo].[UserOrder] ([UserID]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_update_user_account_info_by_userid] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT SELECT ON [dbo].[UserAccount] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_select_user_accounts_by_role] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT UPDATE ON [dbo].[UserAccount] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_select_all_user_address] TO [customer]
 GO
 use [myecommerce]
 GO
-DENY UPDATE ON [dbo].[UserOrder] ([OrderID]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_delete_order] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT UPDATE ON [dbo].[UserOrder] ([PaymentType]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_update_user_account_role_by_userid] TO [customer]
 GO
 use [myecommerce]
 GO
-DENY UPDATE ON [dbo].[UserOrder] ([AdditionalPrice]) TO [customer]
+GRANT EXECUTE ON [dbo].[sp_select_all_user_account] TO [customer]
 GO
 use [myecommerce]
 GO
-GRANT INSERT ON [dbo].[OrderDetail] TO [customer]
+GRANT EXECUTE ON [dbo].[sp_add_order] TO [customer]
 GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[OrderDetail] TO [customer]
-GO
---Change password
 use [myecommerce]
 GO
 GRANT EXECUTE ON [dbo].[sp_change_password] TO [customer]
 GO
-
-
--- Add member in user's role
-ALTER ROLE [customer] ADD MEMBER [user_test]
+use [myecommerce]
 GO
-
---------------------------------
--- Create user employee_table --
---------------------------------
-create login employee_product with password = '1234'
-create user employee_product from login employee_product
+GRANT EXECUTE ON [dbo].[sp_update_user_account_role_by_phone] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_user_address_by_userid] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_account_by_userid] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_order_status_by_orderid] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_useraddress] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_reset_password] TO [customer]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_address_by_useraddressid] TO [customer]
+GO
 
 -- Create role employee
 USE [myecommerce]
 GO
 CREATE ROLE [employee]
 GO
+
+-- Set role employee
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_add_category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_quantity_when_order_cancelled] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_orderdetail_by_orderid_and_productid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT CONTROL ON [dbo].[sp_exist_product_by_productid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_orderdetail] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_delete_product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_add_product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_product] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_products_by_categoryid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_delete_order] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_account_by_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_order_by_status] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_orderdetails_by_productid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_order_status_by_orderid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_orderdetails_by_orderid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_product_by_productid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_user_account_role_by_phone] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_quantity] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_exist_order_by_orderid_and_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_accounts_by_role] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_exist_order_by_orderid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_category_by_categoryid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_discount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_useraddress] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_user_account_info_by_phone] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_quantity_by_amount] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_order] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_order_by_orderid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_delete_all_order_by_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_name] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_exist_category_by_categoryid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_user_account_info_by_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_order_status_by_orderid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_product_quantity_by_productid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_all] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_delete_category] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_order_by_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_unit_price] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_address_by_useraddressid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_user_account_role_by_userid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_orderdetail_by_orderdetailid] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_update_product_description] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_user_account_by_phone] TO [employee]
+GO
+use [myecommerce]
+GO
+GRANT EXECUTE ON [dbo].[sp_select_all_category] TO [employee]
+GO
+
+-- Test
 USE [myecommerce]
 GO
-ALTER AUTHORIZATION ON SCHEMA::[db_owner] TO [employee]
+ALTER ROLE [customer] ADD MEMBER [user_test]
 GO
-
--- Employee's role invole table
-use [myecommerce]
-GO
-GRANT INSERT ON [dbo].[OrderDetail] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[OrderDetail] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT UPDATE ON [dbo].[OrderDetail] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT DELETE ON [dbo].[Category] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT INSERT ON [dbo].[Category] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[Category] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT UPDATE ON [dbo].[Category] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT DELETE ON [dbo].[Product] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT INSERT ON [dbo].[Product] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[Product] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT UPDATE ON [dbo].[Product] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT DELETE ON [dbo].[UserAccount] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT INSERT ON [dbo].[UserAccount] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[UserAccount] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT UPDATE ON [dbo].[UserAccount] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT DELETE ON [dbo].[UserOrder] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT INSERT ON [dbo].[UserOrder] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT SELECT ON [dbo].[UserOrder] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT UPDATE ON [dbo].[UserOrder] TO [employee]
-GO
-
--- Employee's role invole user
-use [myecommerce]
-GO
-GRANT ALTER ON ROLE::[user] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT CONTROL ON ROLE::[user] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT TAKE OWNERSHIP ON ROLE::[user] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT VIEW DEFINITION ON ROLE::[user] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT ALTER ON ROLE::[employee] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT CONTROL ON ROLE::[employee] TO [employee]
-GO
-use [myecommerce]
-GO
-GRANT TAKE OWNERSHIP ON ROLE::[employee] TO [employee]
-GO
-use [myecommerce]
-GO
-DENY VIEW DEFINITION ON ROLE::[employee] TO [employee]
-GO
--- Change password
-use [myecommerce]
-GO
-GRANT EXECUTE ON [dbo].[sp_change_password] TO [employee]
-GO
-
--- Add member in employee's role
-USE [myecommerce]
-GO
-ALTER ROLE [employee] ADD MEMBER [employee_product]
-GO
-
-------------------------------
--- Create user admin_system --
-------------------------------
-create login admin_test with password = '1234'
-create user admin_test from login admin_test
-
-
--- Create role admin_db
-use [myecommerce]
-GO
-CREATE ROLE [admin_db] 
-GO
-
--- Set role admin_db
-use [myecommerce]
-GO
-exec sp_addrolemember db_owner, admin_db
-
--- Add member in admin_db's role
-exec sp_addrolemember admin_db, admin_test
